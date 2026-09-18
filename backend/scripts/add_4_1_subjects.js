e5d60aff77d059ba7a350afcc6533f00b3a6e024
Ravi438 <katteboinaraviteja21@gmai.com>
2026-09-18 18:01:23 +0530

    Finalize portable configuration and environment examples

--- backend/scripts/add_4_1_subjects.js
+++ backend/scripts/add_4_1_subjects.js
@@ -1,4 +1,5 @@
-require('dotenv').config({ path: 'c:/Users/ravit/Desktop/Student Academic Management System/backend/.env' });
+const path = require('path');
+require('dotenv').config({ path: path.resolve(__dirname, '../.env') });
 const mongoose = require('mongoose');
 const Subject = require('../src/modules/academic-master/models/Subject');
 const SubjectBranchMapping = require('../src/modules/academic-master/models/SubjectBranchMapping');
